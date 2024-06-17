791de84605185484ddded4ad3069cd8502f98427
Fabio T. Onishi <ftonishi@hotmail.com>
2024-06-17 20:56:07 -0300

    Atividades da semana 5

---
+++ AppConsole/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AppConsole;
+
+/*---------------------------------------------- */
+Console.Clear();
+Console.WriteLine("Hello, World!");
+Console.ReadKey();
+
+/*---------------------------------------------- */
+Console.Clear();
+Teste teste = new Teste();
+teste.Executar();
+Console.ReadKey();
+
+/*---------------------------------------------- */
+List<Produto> listaProdutos = new List<Produto>();
+ExibeMenu();
+
+void ExibeMenu()
+{
+	var numero=0;
+    Inicio:
+    Console.Clear();
+    Console.WriteLine("Informe o numero: (1) Criar Produto ou (2) Listar Produtos");
+	if (!Int32.TryParse(Console.ReadLine(), out numero))
+	{
+        Console.WriteLine("Informe um numero!");
+        goto Inicio;
+    }
+	
+	switch (numero)
+	{
+		case 1:
+			CriarProduto();
+			break;
+		case 2:
+			ListarProduto();
+			break;
+		default:
+			Console.WriteLine("Escolha uma opção válida!");
+			goto Inicio;
+			break;
+	}
+}
+
+void CriarProduto()
+{
+	string nome;
+	string descricao;
+	double precoUnitario;
+	int quantidade;
+	Console.Clear();
+    Console.WriteLine("Para criar um produto informe:");
+    Console.WriteLine("Nome");
+	nome = Console.ReadLine();
+    Console.WriteLine("Descrição");
+    descricao = Console.ReadLine();
+    Console.WriteLine("Preço Unitário");
+	if (!double.TryParse(Console.ReadLine(), out precoUnitario))
+	{
+		precoUnitario = 0;
+	}
+    Console.WriteLine("Quantidade");
+    if (!int.TryParse(Console.ReadLine(), out quantidade))
+    {
+        quantidade = 0;
+    }    
+    Produto produto = new Produto(nome);
+	produto.Descricao = descricao;
+	produto.PrecoUnitario = precoUnitario;
+	produto.Quantidade = quantidade;
+	listaProdutos.Add(produto);
+    Console.WriteLine("Produto criado com sucesso!");
+    Console.WriteLine("Pressione uma tecla para voltar ao menu. \n");
+	Console.ReadKey();
+	ExibeMenu();
+}
+
+void ListarProduto()
+{
+	Console.Clear();
+    Console.WriteLine("Lista de Produtos:");
+	foreach (var produto in listaProdutos)
+	{
+		Console.WriteLine($"\n Nome: {produto.Nome} Descrição: {produto.Descricao} Preço Unitário {produto.PrecoUnitario} Quantidade {produto.Quantidade}");        
+    }
+    Console.WriteLine("Pressione uma tecla para voltar ao menu. \n");
+    Console.ReadKey();
+    ExibeMenu();
+}
+
+
---
+++ AppConsole/Teste.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsole
+{
+    public class Teste
+    {
+        Endereco endereco;
+        Cliente cliente;
+
+        public Teste()
+        {
+            endereco = new Endereco();
+            endereco.Bairro = "Centro";
+            endereco.Cidade = "S. J. Rio Preto";
+            endereco.Complemento = "Apartamento";
+            endereco.Rua = "Rua Teste";
+            endereco.Numero = 123;
+
+            cliente = new Cliente();
+            cliente.Nome = "Fulano de Tal";
+            cliente.Cpf = "000.000.000-00";
+            cliente.Email = "fulano.tal@teste.com.br";
+            cliente.Profissao = "Docente";
+            cliente.Telefone = "(00) 00000 0000";
+            cliente.Endereco = endereco;          
+        }
+
+        public void Executar()
+        {
+            Console.WriteLine("Cliente");
+            Console.WriteLine("Nome: {0}", cliente.Nome);
+            Console.WriteLine("CPF: {0}", cliente.Cpf);
+            Console.WriteLine("E-mail: {0}", cliente.Nome);
+            Console.WriteLine("Profissão: {0}", cliente.Profissao);
+            Console.WriteLine("Telefone: {0}", cliente.Telefone);
+            Console.WriteLine("Endereço do cliente");
+            Console.WriteLine("Bairro: {0}", cliente.Endereco.Bairro);
+            Console.WriteLine("Cidade: {0}", cliente.Endereco.Cidade);
+            Console.WriteLine("Complemento: {0}", cliente.Endereco.Complemento);
+            Console.WriteLine("Rua: {0}", cliente.Endereco.Rua);
+            Console.WriteLine("Numero: {0}", cliente.Endereco.Numero);
+        }
+    }
+}
